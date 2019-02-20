762aa5928d1ed9f46694bf913f8e8c942a3f2e4b
啊博 <zblove5e@163.com>
2019-02-20 20:26:14 +0800

    新建登录分支，新建登录组件配置路由

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,12 +1,10 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <div>denglu</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import router from './router';
+import Vue from 'vue'
+import App from './App'
+import router from './router'
 // 引入element ui
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI);
 
